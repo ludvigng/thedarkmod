d05f229a5ae8c3d987ddb20bca7e1f51e3955ad2
ludvigng <50339757+ludvigng@users.noreply.github.com>
2022-04-16 18:37:37 +0200

    Update installation path and use local installer
    - Changed install location to use "darkmod" as installation directory, since that seems to be what the game expects.
    - Updated manifest to use local file instead of remote archive on request of developers, since the sha-sum might change on updates.

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [[ ! -d "$XDG_DATA_HOME/TheDarkMod" ]]; then
-  mkdir "$XDG_DATA_HOME/TheDarkMod"
+if [[ ! -d "$XDG_DATA_HOME/darkmod" ]]; then
+  mkdir "$XDG_DATA_HOME/darkmod"
 fi
-if [[ ! -f "$XDG_DATA_HOME/TheDarkMod/tdm_installer.linux64" ]]; then
-  cp /app/bin/tdm_installer.linux64 "$XDG_DATA_HOME/TheDarkMod/"
+if [[ ! -f "$XDG_DATA_HOME/darkmod/tdm_installer.linux64" ]]; then
+  cp /app/bin/tdm_installer.linux64 "$XDG_DATA_HOME/darkmod/"
 fi
 
-cd "$XDG_DATA_HOME/TheDarkMod"
+cd "$XDG_DATA_HOME/darkmod"
 
 if [ "$1" == "install" ] || [[ ! -f ./thedarkmod.x64 ]];then
   exec ./tdm_installer.linux64
