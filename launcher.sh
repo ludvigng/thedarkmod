bccf6d24b1774f6e0b18ed3a0164d464c958d451
ludvigng <50339757+ludvigng@users.noreply.github.com>
2022-04-27 17:07:20 +0200

    Use braces and quotes for path

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [[ ! -d "$XDG_DATA_HOME/darkmod" ]]; then
-  mkdir "$XDG_DATA_HOME/darkmod"
+if [[ ! -d "${XDG_DATA_HOME}/darkmod" ]]; then
+  mkdir "${XDG_DATA_HOME}/darkmod"
 fi
-if [[ ! -f "$XDG_DATA_HOME/darkmod/tdm_installer.linux64" ]]; then
-  cp /app/bin/tdm_installer.linux64 "$XDG_DATA_HOME/darkmod/"
+if [[ ! -f "${XDG_DATA_HOME}/darkmod/tdm_installer.linux64" ]]; then
+  cp /app/bin/tdm_installer.linux64 "${XDG_DATA_HOME}/darkmod/"
 fi
 
-cd "$XDG_DATA_HOME/darkmod"
+cd "${XDG_DATA_HOME}/darkmod"
 
 if [ "$1" == "install" ] || [[ ! -f ./thedarkmod.x64 ]];then
   exec ./tdm_installer.linux64
