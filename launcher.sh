a638b2b58e2e5f80c58a5cfe8102a86518182de1
ludvigng <50339757+ludvigng@users.noreply.github.com>
2022-05-06 17:20:07 +0200

    Suggestions from shellcheck
    Add || exit to cd command in case of permission error.

--- launcher.sh
+++ launcher.sh
@@ -6,7 +6,7 @@ if [[ ! -f "${XDG_DATA_HOME}/darkmod/tdm_installer.linux64" ]]; then
   cp /app/bin/tdm_installer.linux64 "${XDG_DATA_HOME}/darkmod/"
 fi
 
-cd "${XDG_DATA_HOME}/darkmod"
+cd "${XDG_DATA_HOME}/darkmod" || exit
 
 if [ "$1" == "install" ] || [[ ! -f ./thedarkmod.x64 ]];then
   exec ./tdm_installer.linux64
